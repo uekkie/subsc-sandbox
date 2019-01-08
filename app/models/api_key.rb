1a8991af13de0073ace6b5fd8716636ec4097298
Hiroshi Ueki <hirocueki@gmail.com>
2019-01-08 15:44:09 +0900

    Token作成処理の実装

--- app/models/api_key.rb
+++ app/models/api_key.rb
@@ -1,2 +1,21 @@
 class ApiKey < ApplicationRecord
+  attr_accessible :access_token, :expires_at, :user_id, :active, :application
+  before_create :generate_access_token
+  before_create :set_expiration
+  belongs_to :user
+
+  def expired?
+    DateTime.now >= self.expires_at
+  end
+
+  private
+  def generate_access_token
+    begin
+      self.access_token = SecureRandom.hex
+    end while self.class.exists?(access_token: access_token)
+  end
+
+  def set_expiration
+    self.expires_at = DateTime.now+30
+  end
 end
